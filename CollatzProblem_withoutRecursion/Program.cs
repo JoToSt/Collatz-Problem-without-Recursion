911bd4cb79e483f5491ff94e3bbbb49251708ffe
Joanna T.Strojny <joanna.t.strojny@gmail.com>
2022-08-02 22:25:48 +0200

    Collatz problem programmed without using recursion

---
+++ CollatzProblem_withoutRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CollatzProblem_withoutRecursion
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Please enter the number s: ");
+            int s = int.Parse(Console.ReadLine());
+            int n = 0;
+            while (s != 1)
+            {
+                if ( s % 2  == 0)
+                    s = s/2;
+                else
+                    s = 3 * s + 1  ;
+                n++;
+            }
+            
+            Console.WriteLine($"The number of steps:  {n}\n\n");
+
+
+            Console.ReadKey();
+        }
+    }
+}
